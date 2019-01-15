08a0676972396ad8de4429b5ab82e79b32d0d1d2
keisukekodama <myzk.kodama@gmail.com>
2019-01-15 18:15:32 +0900

    second

--- app/assets/javascripts/user.js
+++ app/assets/javascripts/user.js
@@ -1,6 +1,6 @@
 
 $(function() {
-  var search_list = $("#user-search-result");
+  var searchList = $("#user-search-result");
 function appendIndex(index){
   var html =`
 <div class="chat-group-user clearfix">
@@ -8,7 +8,7 @@ function appendIndex(index){
   <a class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id=${index.id} data-user-name=${index.name}>追加</a>
 </div>
 `
-search_list.append(html);
+searchList.append(html);
 }
 function appendNoIndex(index) {
     var html = `
@@ -16,11 +16,11 @@ function appendNoIndex(index) {
 `
     search_list.append(html);
   }
-  function addIndexToGroup(user_id, user_name) {
+  function addIndexToGroup(userId, userName) {
     var html = `
   <div class='chat-group-user clearfix js-chat-member'>
-  <input name='group[user_ids][]' type='hidden' value='${user_id}'>
-  <p class='chat-group-user__name'>${user_name}</p>
+  <input name='group[user_ids][]' type='hidden' value='${userId}'>
+  <p class='chat-group-user__name'>${userName}</p>
   <a class='user-search-remove chat-group-user__btn chat-group-user__btn--remove js-remove-btn'>削除</a>
 </div>
 `
@@ -35,14 +35,14 @@ function appendNoIndex(index) {
       dataType: 'json'
     })
   .done(function(indexs) {
-    search_list.empty();
+    searchList.empty();
      if (indexs.length !== 0) {
        indexs.forEach(function(index){
          appendIndex(index);
        });
      }
      else {
-      search_list.empty();
+      searchList.empty();
        appendNoIndex("一致するユーザーはいません");
      }
    })
@@ -50,10 +50,10 @@ function appendNoIndex(index) {
     alert('ユーザーの検索に失敗しました')
   });
 });
-  search_list.on("click",".user-search-add",function(){
-    var user_id = $(this).attr("data-user-id");
-    var user_name = $(this).attr("data-user-name");
-addIndexToGroup(user_id, user_name);
+  searchList.on("click",".user-search-add",function(){
+    var userId = $(this).attr("data-user-id");
+    var userName = $(this).attr("data-user-name");
+addIndexToGroup(userId, userName);
 $(this).parent().remove();
   })
   $("#chat-group-users").on("click",".user-search-remove",function(){
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,7 +7,6 @@ class UsersController < ApplicationController
   def index
     @indexs = User.where("name LIKE(?) AND (id != ?)","%#{params[:keyword]}%", current_user.id).limit(20)
     respond_to do |format|
-     format.html
      format.json
   end
 end
