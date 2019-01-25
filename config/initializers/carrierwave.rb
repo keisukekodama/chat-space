1b89ff7daa3e1d87f9f8fb5ee609e58d47a066f5
keisukekodama <myzk.kodama@gmail.com>
2019-01-25 12:44:48 +0900

    ''

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -9,10 +9,10 @@ CarrierWave.configure do |config|
     provider: 'AWS',
     aws_access_key_id: Rails.application.secrets.aws_access_key_id,
     aws_secret_access_key: Rails.application.secrets.aws_secret_access_key,
-    region: ap-northeast-1
+    region: 'ap-northeast-1'
   }
 
-  config.fog_directory  = chat-keisuke
+  config.fog_directory  = 'chat-keisuke'
   config.asset_host = 'https://s3-ap-northeast-1
   }.amazonaws.com/chat-keisuke'
 end
